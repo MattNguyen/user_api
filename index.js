83a7342b60ad7a94a2bc152582e3ecf5e18b94ed
Matt Nguyen <mnguyen916@gmail.com>
2014-07-27 16:57:13 -0700

    Define routes for User API

--- index.js
+++ index.js
@@ -1,6 +1,32 @@
 var Hapi = require('hapi');
-var server = new Hapi.Server(8080, 'localhost')
+var server = new Hapi.Server(8080, 'localhost');
+
+server.route([{
+  path: '/api/{version}/users',
+  method: 'POST',
+  handler: function(request, reply) {
+
+  }
+}, {
+  path: '/api/{version}/users/{id}',
+  method: 'GET',
+  handler: function(request, reply) {
+
+  }
+}, {
+  path: '/api/{version}/users/{id}',
+  method: 'PUT',
+  handler: function(request, reply) {
+
+  }
+}, {
+  path: '/api/{version}/users/{id}',
+  method: 'DELETE',
+  handler: function(request, reply) {
+
+  }
+}]);
 
 server.start(function() {
-  console.log('Server started @ ' + server.info.uri)
-})
+  console.log('Server started @ ' + server.info.uri);
+});
