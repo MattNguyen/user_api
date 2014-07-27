c30fdfe5aa974c4e98b7332e424b6dc5ba650c16
Matt Nguyen <mnguyen916@gmail.com>
2014-07-27 16:56:04 -0700

    Add gulp with dev task for linting and server start

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+var nodemon = require('gulp-nodemon');
+
+gulp.task('lint', function() {
+  gulp.src(['./**/*.js', '!./node_modules/**'])
+  .pipe(jshint())
+  .pipe(jshint.reporter('default'));
+});
+
+gulp.task('dev', function() {
+  nodemon({
+    script: './index.js',
+    ignore: ['gulpfile.js', 'node_modules']
+  })
+  .on('change', ['lint']);
+});
